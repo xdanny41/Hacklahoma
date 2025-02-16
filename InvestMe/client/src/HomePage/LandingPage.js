c6eb05440b6e56fce68dc94d299e045e706c8838
fluffybunni <motohzx@gmail.com>
2025-02-15 22:11:44 -0600

    Implemented watchlist

--- InvestMe/client/src/HomePage/LandingPage.js
+++ InvestMe/client/src/HomePage/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NewsFeed from '../HomePage/NewsFeed';
+import Watchlist from '../HomePage/Watchlist';
 
 function LandingPage() {
   // Sample data to simulate “tweets” about stocks
@@ -28,18 +29,20 @@ function LandingPage() {
   return (
     <div className="container-fluid vh-100">
       <div className="row h-100">
-        {/* LEFT SIDEBAR */}
-        <div className="col-2 bg-light d-flex flex-column align-items-start py-3">
+        {/* LEFT SIDEBAR WITH WATCHLIST */}
+        <div className="col-3 bg-light d-flex flex-column align-items-start py-3">
           <h4 className="mb-4">Logo/Brand</h4>
           <nav className="nav flex-column">
             <a className="nav-link active" href="#home">Home</a>
             <a className="nav-link" href="#profile">Profile</a>
             <a className="nav-link" href="#settings">Settings</a>
           </nav>
+          <h6 className="mt-4"> </h6>
+          <Watchlist />
         </div>
         
         {/* CENTER FEED */}
-        <div className="col-7 border-start border-end" style={{ overflowY: 'auto' }}>
+        <div className="col-6 border-start border-end" style={{ overflowY: 'auto' }}>
           <div className="py-3 px-3">
             <h5>Home</h5>
           </div>
