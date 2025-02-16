39adc74dc60d7ae8a8a9f0f224a6b3d5e1c49816
xdanny41 <93782295+xdanny41@users.noreply.github.com>
2025-02-16 04:59:49 -0600

    temp

--- InvestMe/client/src/App.js
+++ InvestMe/client/src/App.js
@@ -2,21 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthTest from './HomePage/AuthTest';
 import LandingPage from './HomePage/LandingPage';
 import Feed from './HomePage/Feed';
 import Createpost from './HomePage/Createpost';
 import PostThread from './HomePage/PostThread'; // Adjust the path accordingly
 import Portfolio from './HomePage/UserPortfolio';
 import CompanyPage from './HomePage/CompanyPage';
+import Navbar from './HomePage/navbar';
 
 
 function App() {
   return (
     <div>
-      <LandingPage />
-    <CompanyPage></CompanyPage>
+
        <Router>
+       <Navbar></Navbar>
+       <LandingPage />
       <Routes>
       <Route path="/" element={<Feed />} /> 
         <Route path="/post/:id" element={<PostThread />} />
